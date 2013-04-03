3ab075a6c45b077681f90c0e89d5483ada253cae
amr fawzy mohamed <amrfawzymoh_88@yahoo.com>
2013-04-03 13:51:54 +0200

    welcome message

---
+++ application/views/welcomepage.php
@@ -0,0 +1,7 @@
+<b>أهلا بك في تاكسي مصر</b></br></br>
+<form action="<?php echo base_url('search'); ?>" method="post">
+أدخل رقم التاكسي: <br><br>
+<input type="text" name="taxinum"><br><br>
+<input type="submit" name="search" value="أبحث عن التاكسي"><br><br>
+</form>
+
