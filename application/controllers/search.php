bfc00b1f449a6a9ecd995d17a82caead8c86ff8a
KareemKamal <kareem_kamal_aref@yahoo.com>
2013-04-04 11:05:08 +0300

    fixed by Kareem&Barghash  KB team

--- application/controllers/search.php
+++ application/controllers/search.php
@@ -4,15 +4,10 @@ class Search extends CI_Controller {
 
     public function __construct() {
         parent::__construct();
-<<<<<<< HEAD
-        $this->load->model(array('taxis'));
         $this->load->helper('url');
         $this->load->library('session');
-        
-        
-=======
         $this->load->model(array('taxis','reviews'));
->>>>>>> 661a7681025985e18d872e26d9a00615efba894f
+
     }
 	public function index(){
 	$taxinum=$this->input->post('taxinum');
@@ -20,7 +15,7 @@ class Search extends CI_Controller {
         if(isset($_SESSION['username']))
         {
             $taxi_id=$this->taxi_exist();
-			echo $taxi_id;
+		//	echo $taxi_id;
             if($taxi_id){
             $this->rate_taxi($taxi_id);
             }else{
@@ -30,57 +25,30 @@ class Search extends CI_Controller {
 	}
 
     public function rate_taxi($taxi_id) {
-<<<<<<< HEAD
-=======
-$taxinum=$this->input->post('taxinum');
+	$taxinum=$this->input->post('taxinum');
         $data['taxiscore'] = $this->taxis->get_score($taxi_id); //MVC model
         $data['taxireview'] = $this->reviews->get_reviews($taxi_id); //an associative array contains all 6 items
-$data['taxiname']=$taxinum;
-$data['taxiID']=$taxi_id;
-$data['count']=$this->reviews->get_count($taxi_id);;
+	$data['taxiname']=$taxinum;
+	$data['taxiID']=$taxi_id;
+	$data['count']=$this->reviews->get_count($taxi_id);;
         $this->load->view('search_logged', $data); //associative array with two values ,first is assoc and the other is score
     }
 
     public function add_taxi($key){
             $keyword['searchKey'] = $key;
             $this->load->view("noMatch",$keyword);
-        }
->>>>>>> 661a7681025985e18d872e26d9a00615efba894f
-
-        $data['taxiscore'] = $this->taxis->get_score();  //MVC model
-		$data['taxireview'] = $this->reviews->get_reviews(); //an associative array contains all 6 items
-        $this->load->view('search_logged', $data); //associative array with two values ,first is assoc and the other is score 
     }
 
-<<<<<<< HEAD
-	
-	public function taxi_exist() {
-		$taxinum=$this->input->post('taxinum');
-        $taxi_id = $this->taxis->found($taxinum); 
-
-=======
+       
+	     
     public function taxi_exist() {
         $taxinum=$this->input->post('taxinum');
         $taxi_id = $this->taxis->found($taxinum);
 		//echo $taxi_id; 
->>>>>>> 661a7681025985e18d872e26d9a00615efba894f
        return $taxi_id;
-
     }
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 	
-	isset($_SESSION['username']){
-	$taxi_id=taxi_exist();
-	if($taxi_id){
-	rate_taxi($taxi_id);
-	
-	}else{
-		header("Location: /add_taxi/$taxinum");
-	
-	}
-	
-	}
 	
 	public function not_found() {
         $this->load->view("template-top");
@@ -122,37 +90,9 @@ $data['count']=$this->reviews->get_count($taxi_id);;
 	} 
    
  	
-}
+   }
 }
 ?>
-=======
-<<<<<<< HEAD
-=======
-
->>>>>>> 0e20e5638c306796332f93528723db05a8f3bc3d
 
 
 
-public function index(){
-        if(isset($_SESSION['username']))
-        {
-            $taxi_id=$taxi->taxi_exist();
-            if($taxi_id){
-            $taxi->rate_taxi($taxi_id);
-            }else{
-            $taxi->add_taxi($taxinum);
-            }
-    }
-}
-}
-
-	
-	
-
-?>
-<<<<<<< HEAD
->>>>>>> some errors fixed
->>>>>>> 661a7681025985e18d872e26d9a00615efba894f
-=======
-
->>>>>>> 0e20e5638c306796332f93528723db05a8f3bc3d
